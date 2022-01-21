dc018b121441805a397d3d8c303b14d096bc2694
khazixi <khazixfangirl@gmail.com>
2022-01-20 20:11:59 -0500

    The game **should be runnable but lacks key features

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import random
 import enum
 from typing import Union, Tuple, Optional
 
-class Color(enum.Enum):
+
+"""
+This is a simple project that I have made to test my skills in python.
+I have not made a completed project in python in a long time so this seemed like a good oppurtunity
+The logic is simple and the game can be refactored to become extensible
+"""
+
+# TODO: Add Color Changer Cards
+# TODO: Add Skip and Reverse Cards
+# TODO: Add Draw Cards
+# TODO: Create graphics for the game
+
+class Color(enum.Enum):  # Enum is used to assign colors 
     RED = enum.auto()
     BLUE = enum.auto()
     GREEN = enum.auto()
@@ -10,15 +22,15 @@ class Color(enum.Enum):
 
 
 class Card:
-    def __init__(self, name, color) -> None:
-        self._name = name
+    def __init__(self, number, color) -> None:
+        self._name = number
         self._color = color
 
     def get_color(self):
         return self._color
 
     def get_number(self) -> str:
-        return self._name
+        return self._number
 
 class Stack:
     def __init__(self):
@@ -52,11 +64,11 @@ class Player:
     def __init__(self) -> None:
         self.hand: list[Card] = []
 
-    def add_card(self, cards: Tuple[Card]) -> None:
-        if len(cards) == 1:
-            self.hand.append(cards)
-        else: 
+    def add_card(self, cards: Tuple[Card] | Card) -> None:
+        if type(cards) is tuple:
             self.hand.extend(card for card in cards)
+        elif type(cards) is Card:
+            self.hand.append(cards)
 
     def show_hand(self, card: Card) -> Tuple[Card, ...]:
         return tuple(card for card in self.hand)
@@ -66,3 +78,84 @@ class Player:
             raise IndexError("Cannot Use Card if no card is in hand!")
         choice = int("Pick a number between 1~")
         return self.hand[choice]
+
+
+class Game:
+    def __init__(self):
+        self.deck = Deck()
+        self.stack = Stack()
+        self.player_list: list[Player] = []
+        # Initializer functions will be added to __init__
+        self.create_players()
+        self.initialize_stack()
+        self.create_players()
+
+    ###### GAME INITIALIZER FUNCTIONS ######
+
+    def create_cards(self):
+        for color in Color:
+            for number in range(10):
+                for _ in range(2):
+                    self.deck.add_card(Card(number, color))
+
+    def initialize_stack(self):
+        random.shuffle(self.deck)
+        card = random.choice(self.deck)
+        self.stack.add_stack(card)
+
+    def create_players(self):
+        while True:
+            choice = input("How many players would like to play?")
+            if choice.isnumeric() and 0 < int(choice) < 8:
+                self.player_list.extend(Player() for _ in range(int(choice)))
+                for player in self.player_list:
+                    player.add_card(self.deck.draw_card(5))
+                break
+
+    ###### GAME LOGIC FUNCTIONS ######
+    def check_hand(self, player: Player, stack_card: Card) -> list[bool]:
+        bool_chain = []
+        for card in player.hand:
+            if stack_card.get_color() == card.get_color():
+                bool_chain.append(True)
+            elif stack_card.get_number == card.get_number():
+                bool_chain.append(True)
+            else:
+                bool_chain.append(False)
+
+        return bool_chain
+
+    
+    def check_card(self, card: Card, stack_card: Card) -> bool:
+        if card.get_color() == stack_card.get_color():
+            return True
+        elif card.get_color() == stack_card.get_color():
+            return True
+        else:
+            return False
+        
+    ###### THE FUNCTION THAT RUNS THE ACTUAL GAME ######
+    def run(self):  # This is where the game logic will occur!
+        while True:
+            for player in self.player_list:
+                stack_card = self.stack.get_top()
+                while True:
+                    if any(self.check_hand(player, stack_card)):
+                        player_card = player.use_card()
+                        if self.check_card(player_card, stack_card):
+                            self.stack.add_stack(player_card)
+                            break
+                        else:
+                            # This returns the card to hand if it cannot be placed on the top stack card
+                            player.add_card(player_card)
+                        # This is meant to check if the player's hand is empty and then they win
+                        if not player.show_hand():
+                            winner = player
+                            break
+                    else:
+                        player.add_card(self.deck.draw_card()) 
+
+
+if __name__ == "__main__":
+    uno = Game()
+    uno.run()
